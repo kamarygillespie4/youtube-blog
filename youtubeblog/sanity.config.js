b454e2ef705aecd525a8c1ac1b97438b16b6f24f
Kamary <gillespiekamary@gmail.com>
2023-02-21 12:20:44 -0700

    testing netlify

--- youtubeblog/sanity.config.js
+++ youtubeblog/sanity.config.js
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {dashboardTool} from '@sanity/dashboard'
+import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,23 @@ export default defineConfig({
   projectId: 'royty8xz',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Sanity Studio',
+              apiId: '274dc56c-5d18-4747-83eb-126913a6e6c0',
+              buildHookId: '63f5191e41f72b065c6adb59',
+              name: 'lucky-moxie-2fc34a',
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
